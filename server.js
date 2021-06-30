9888d57c820fbe01aa1bcdb1708b1cdb8e5744fc
sbuDiction <nsbu68501@gmail.com>
2021-06-30 20:13:46 +0200

    deploying

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
-// app.use(app.static('build'));
+app.use(express.static('client/build'));
 const PORT = process.env.PORT || 5000;
 
 const dbQueries = Db_queries(pool);
