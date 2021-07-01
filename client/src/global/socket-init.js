054e3da326949eabc7d5b64d6eef1391aed50b68
sbuDiction <nsbu68501@gmail.com>
2021-07-01 23:25:12 +0200

    fixing socket io issues

--- client/src/global/socket-init.js
+++ client/src/global/socket-init.js
@@ -1,14 +1,14 @@
 import { Component } from 'react';
 import socketIOClient from 'socket.io-client';
 
-let socket;
+const socket = socketIOClient();
 class SocketConnection extends Component {
     constructor() {
         super();
         this.state = {
             endpoint: "http://localhost:5000",
         }
-        socket = socketIOClient(this.state.endpoint);
+        // socket = socketIOClient(this.state.endpoint);
     }
 
     render() {
